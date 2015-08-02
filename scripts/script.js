faae7b6bbdb5879cb8d5f43e85326026849561af
Stefan <stefanpiazza@gmail.com>
2015-08-02 14:32:03 +0100

    Added mouse events.

--- scripts/script.js
+++ scripts/script.js
@@ -23,13 +23,14 @@
 
         isTouchStart: false,
         stickySwipe: true,
+        isMoving: false,
 
         loop: true,
         transitionSpeed: 300,
 
         intervalObj: null,
 
-        __initCarousel: function(){
+        __initCarousel: function() {
             //scroll the window up to hide the address bar of the browser.
             window.setTimeout(function() { window.scrollTo(0, 1); }, 100);
 
@@ -43,7 +44,7 @@
             carousel.panelContainer.style.width = carousel.panelsX * carousel.panelWidth + 'px';
             carousel.panelContainer.style.height = carousel.panelHeight + 'px';
 
-            for (var i = 0; i < carousel.panels.length; i++){
+            for (var i = 0; i < carousel.panels.length; i++) {
                 carousel.panels[i].style.width = carousel.panelWidth + 'px';
                 carousel.panels[i].style.height = carousel.panelHeight + 'px';
             }
@@ -53,19 +54,30 @@
             this.__initEvents();
         },
 
-        __initEvents: function(){
+        __initEvents: function() {
             carousel.wrapper.addEventListener('touchstart', this.startHandler.bind(this), false);
             carousel.wrapper.addEventListener('touchmove', this.moveHandler.bind(this), false);
             carousel.wrapper.addEventListener('touchend', this.endHandler.bind(this), false);
+
+            carousel.wrapper.addEventListener('mousedown', this.startHandler.bind(this), false);
+            carousel.wrapper.addEventListener('mousemove', this.moveHandler.bind(this), false);
+            carousel.wrapper.addEventListener('mouseup', this.endHandler.bind(this), false);           
+            carousel.wrapper.addEventListener('mouseleave', this.endHandler.bind(this), false);           
 
             if (carousel.loop){
                 carousel.panelContainer.addEventListener('webkitTransitionEnd', this.handleTransitionEnd.bind(this), false);
                 this.handleTransitionEnd();
             }
+
+            else {
+                carousel.addEventListener('webkitTransitionEnd', function () {
+                    carousel.isMoving = false;
+                }.bind(this), false);
+            }
         },
 
-        startHandler: function(event){
-            carousel.startX = event.touches[0].pageX;
+        startHandler: function(event) {
+            carousel.startX = (event.touches ? event.touches[0].pageX : event.pageX);
             carousel.timer = setInterval(function() { carousel.timerCounter++; }.bind(this), 10);
             carousel.isTouchStart = true;
 
@@ -74,11 +86,11 @@
             this.panelStartX = carousel.distanceX + carousel.currentDistanceX;
         },
 
-        moveHandler: function(event){
-            if (carousel.isTouchStart){
+        moveHandler: function(event) {
+            if (carousel.isTouchStart && carousel.isMoving === false) {
                 carousel.panelContainer.style.webkitTransitionDuration = '0';
 
-                carousel.distanceX = event.touches[0].pageX - carousel.startX;
+                carousel.distanceX = (event.touches ? event.touches[0].pageX : event.pageX) - carousel.startX;
 
                 if (carousel.stickySwipe == true){
                     carousel.panelContainer.style.webkitTransform = 'translate3d(' + (carousel.distanceX + carousel.currentDistanceX) + 'px, 0px, 0px)';
@@ -88,7 +100,7 @@
             }
         },
 
-        endHandler: function(event){
+        endHandler: function(event) {
             clearInterval(carousel.timer);
 
             if (carousel.distanceX > 0) { carousel.directionX = 'right'; }
@@ -98,19 +110,19 @@
                 this.comeBack();
             }
 
-            else if (carousel.timerCounter < 30 && carousel.distanceX > 10){
+            else if (carousel.timerCounter < 30 && carousel.distanceX > 10) {
                 this.moveRight();
             }
 
-            else if (carousel.timerCounter < 30 && carousel.distanceX < -10){
+            else if (carousel.timerCounter < 30 && carousel.distanceX < -10) {
                 this.moveLeft();
             }
 
-            else if (carousel.distanceX <= -(carousel.panelWidth / 2)){
+            else if (carousel.distanceX <= -(carousel.panelWidth / 2)) {
                 this.moveLeft();
             }
 
-            else if (carousel.distanceX >= (carousel.panelWidth / 2)){
+            else if (carousel.distanceX >= (carousel.panelWidth / 2)) {
                 this.moveRight();
             }
 
@@ -118,7 +130,7 @@
                 this.comeBack();
             }
 
-            if(Math.abs(carousel.distanceX) < 10 || Math.abs(carousel.distanceY) < 10) {
+            if (Math.abs(carousel.distanceX) === 0) {
                 var elementId = event.target.getAttribute('id');
             }
 
@@ -127,40 +139,47 @@
             carousel.distanceX = 0;
         },
 
-        handleTransitionEnd:function(){
+        handleTransitionEnd:function() {
             if (carousel.currentDistanceX === -(carousel.maxDistanceX - carousel.panelWidth)) {
                 carousel.panelContainer.appendChild(carousel.panels[0]);
-                carousel.panelContainer.style.webkitTransitionDuration = '0';
+                carousel.panelContainer.style.webkitTransitionDuration = '0ms';
                 carousel.panelContainer.style.webkitTransform = 'translateX(-' + (carousel.maxDistanceX - 2*carousel.panelWidth) + 'px)';
                 carousel.currentDistanceX = -(carousel.maxDistanceX - 2*carousel.panelWidth);
             }
 
             if (carousel.currentDistanceX === 0) {
                 carousel.panelContainer.insertBefore(carousel.panels[carousel.panels.length - 1],carousel.panels[0]);
-                carousel.panelContainer.style.webkitTransitionDuration = '0';
+                carousel.panelContainer.style.webkitTransitionDuration = '0ms';
                 carousel.panelContainer.style.webkitTransform = 'translateX(-' + carousel.panelWidth + 'px)';
                 carousel.currentDistanceX = -carousel.panelWidth;
             }
+
+            carousel.isMoving = false;
         },
 
         moveLeft: function () {
+            carousel.isMoving = true;
+
             carousel.currentDistanceX += -carousel.panelWidth;
             carousel.panelContainer.style.webkitTransitionDuration = carousel.transitionSpeed + 'ms';
             carousel.panelContainer.style.webkitTransform = 'translate3d(' + carousel.currentDistanceX + 'px, 0,0)';
         },
 
         moveRight: function () {
+            carousel.isMoving = true;
+
             carousel.currentDistanceX += carousel.panelWidth;
             carousel.panelContainer.style.webkitTransitionDuration =  carousel.transitionSpeed + 'ms';
             carousel.panelContainer.style.webkitTransform = 'translate3d(' + carousel.currentDistanceX + 'px, 0,0)';
         },
 
-        comeBack: function(){
+        comeBack: function() {
             carousel.panelContainer.style.webkitTransitionDuration = 250 + 'ms';
             carousel.panelContainer.style.webkitTransitionTimingFunction = 'ease-out';
             carousel.panelContainer.style.webkitTransform = 'translate3d(' + carousel.currentDistanceX + 'px, 0,0)';
         },
     };
+
     window.carousel = carousel;
 })();
 
